7f298b3fd1a17af088a5fa2b9275161bc4758ac2
Gabriel Hernandez <ghernand@teksystems.com>
2021-06-23 12:38:21 -0400

    change /checkout path to /

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/checkout",
+    path: "/",
     name: "checkout",
     component: () => import("../views/Checkout.vue"),
   },
